6642f809d5fdda0ef028f2e396f9406d1faaf744
Tormod <tormod.fjeldskaar@gmail.com>
2011-03-02 22:06:57 +0100

    Basic rules of ten-pin bowling implemented.

---
+++ csharp/Bowling/Bowling.Tests/GameSpecs.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bowling.Tests
+{
+	[TestClass]
+	public class When_a_new_game_is_created : Shared
+	{
+		[TestMethod]
+		public void Should_give_all_players_zero_score()
+		{
+			Assert.AreEqual(0, game.Score[player1]);
+			Assert.AreEqual(0, game.Score[player2]);
+		}
+
+		[TestMethod]
+		public void Should_start_with_first_player()
+		{
+			Assert.AreEqual(player1, game.CurrentPlayer);
+		}
+	}
+
+	[TestClass]
+	public class When_first_player_knocks_three_pins : Shared
+	{
+		protected override void AfterInit()
+		{
+			base.AfterInit();
+			game.RegisterPinsKnockedOver(3);
+		}
+
+		[TestMethod]
+		public void Should_not_score_any_points_yet()
+		{
+			Assert.AreEqual(0, game.Score[player1]);
+		}
+
+		[TestMethod]
+		public void Should_give_first_player_second_attempt()
+		{
+			Assert.AreEqual(player1, game.CurrentPlayer);
+		}
+	}
+	
+	[TestClass]
+	public class When_first_player_knocks_three_pins_twice : Shared
+	{
+		protected override void AfterInit()
+		{
+			base.AfterInit();
+			game.RegisterPinsKnockedOver(3);
+			game.RegisterPinsKnockedOver(3);
+		}
+
+		[TestMethod]
+		public void Should_score_six_points()
+		{
+			Assert.AreEqual(6, game.Score[player1]);
+		}
+
+		[TestMethod]
+		public void Should_advance_to_next_player()
+		{
+			Assert.AreEqual(player2, game.CurrentPlayer);
+		}
+	}
+
+	[TestClass]
+	public class When_game_progresses : Shared
+	{
+		[TestMethod]
+		public void Should_report_appropriate_players_and_scores()
+		{
+			var testCases = new[]
+			{
+				new { KnockedPins = "6", ExpectedScores = "0-0", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-2", ExpectedScores = "8-0", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-2 - 3", ExpectedScores = "8-0", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-2 - 3-4", ExpectedScores = "8-7", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-2 - 3-4 - 10", ExpectedScores = "8-7", ExpectedPlayer = player2 },
+
+				new { KnockedPins = "6-4", ExpectedScores = "0-0", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-4 - 3-3", ExpectedScores = "0-6", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-4 - 3-3 - 5", ExpectedScores = "15-6", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5", ExpectedScores = "15-6", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5 - 10", ExpectedScores = "15-6", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5 - 10 - 10", ExpectedScores = "35-6", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5 - 10 - 10 - 10", ExpectedScores = "35-6", ExpectedPlayer = player1 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5 - 10 - 10 - 10 - 3-5", ExpectedScores = "61-6", ExpectedPlayer = player2 },
+				new { KnockedPins = "6-4 - 3-3 - 5-5 - 10 - 10 - 10 - 3-5 - 10", ExpectedScores = "61-36", ExpectedPlayer = player2 },
+			};
+
+			foreach (var game in testCases)
+			{
+				NewGame();
+				Play(game.KnockedPins, game.ExpectedScores, game.ExpectedPlayer);
+			}
+		}
+
+		private void Play(string knockedPins, string expectedScores, string expectedPlayer)
+		{
+			Play(knockedPins
+				.Split('-')
+				.Select(s => Convert.ToInt32(s)));
+
+			AssertExpectedScores(expectedScores
+				.Split('-')
+				.Select(s => Convert.ToInt32(s))
+				.ToArray());
+
+			AssertExpectedPlayer(expectedPlayer);
+		}
+
+		private void Play(IEnumerable<int> attempts)
+		{
+			foreach (var knockedPins in attempts)
+			{
+				game.RegisterPinsKnockedOver(knockedPins);
+			}
+		}
+
+		private void AssertExpectedScores(int[] scores)
+		{
+			Assert.AreEqual(scores[0], game.Score[player1]);
+			Assert.AreEqual(scores[1], game.Score[player2]);
+		}
+
+		private void AssertExpectedPlayer(string expectedPlayer)
+		{
+			Assert.AreEqual(expectedPlayer, game.CurrentPlayer);
+		}
+	}
+
+	public abstract class Shared
+	{
+		protected Game game;
+
+		protected string player1;
+		protected string player2;
+
+		[TestInitialize]
+		public void TestInit()
+		{
+			InitFields();
+			BeforeInit();
+
+			NewGame();
+
+			AfterInit();
+		}
+
+		protected void NewGame()
+		{
+			game = new Game(player1, player2);
+		}
+
+		private void InitFields()
+		{
+			player1 = "Alice";
+			player2 = "Bob";
+		}
+
+		protected virtual void BeforeInit()
+		{
+		}
+
+		protected virtual void AfterInit()
+		{
+		}
+	}
+}
---
+++ csharp/Bowling/Bowling/Game.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+	public class Game
+	{
+		private Dictionary<string, int> playerScores;
+
+		private Dictionary<string, int> pendingBonuses;
+
+		private Dictionary<string, int> pendingBonusPoints;
+
+		private int currentPlayer;
+
+		private int currentFrame;
+
+		private int currentAttempt;
+
+		private int pinsRemaining;
+
+		public Game(params string[] players)
+		{
+			playerScores = players.ToDictionary(p => p, p => 0);
+			pendingBonuses = players.ToDictionary(p => p, p => 0);
+			pendingBonusPoints = players.ToDictionary(p => p, p => 0);
+			currentPlayer = 0;
+			currentAttempt = 0;
+			pinsRemaining = 10;
+		}
+
+		public IDictionary<string, int> Score
+		{
+			get { return playerScores.ToDictionary(x => x.Key, x => x.Value); }
+		}
+
+		public string CurrentPlayer
+		{
+			get { return playerScores.Keys.ElementAt(currentPlayer); }
+		}
+
+		public bool FirstAttempt
+		{
+			get { return currentAttempt == 0; }
+		}
+
+		private bool CurrentPlayerHasNoMoreAttempts
+		{
+			get { return currentAttempt == 1; }
+		}
+
+		public bool AllPinsAreKnockedDown
+		{
+			get { return pinsRemaining == 0; }
+		}
+
+		public bool CurrentFrameHasNoMorePlayers 
+		{
+			get { return currentPlayer == playerScores.Count - 1; }
+		}
+
+		public bool CurrentPlayerHasPendingBonuses 
+		{
+			get { return pendingBonuses[CurrentPlayer] > 0; }
+		}
+
+		public void RegisterPinsKnockedOver(int pinsKnockedOver)
+		{
+			pinsRemaining = Math.Max(0, pinsRemaining - pinsKnockedOver);
+
+			if (CurrentPlayerHasPendingBonuses)
+			{
+				pendingBonusPoints[CurrentPlayer] += pinsKnockedOver;
+
+				if (--pendingBonuses[CurrentPlayer] == 0)
+				{
+					playerScores[CurrentPlayer] += pendingBonusPoints[CurrentPlayer];
+				}
+			}
+
+			if (CurrentPlayerHasNoMoreAttempts)
+			{
+				AdvanceToNextPlayer();
+			}
+			else
+			{
+				if (AllPinsAreKnockedDown)
+				{
+					AdvanceToNextPlayer();
+				}
+				else
+				{
+					AdvanceToNextAttempt();
+				}
+			}
+		}
+
+		private void UpdateCurrentPlayerScore()
+		{
+			if (AllPinsAreKnockedDown)
+			{
+				if (FirstAttempt)
+				{
+					RegisterStrikeForCurrentPlayer();
+				}
+				else
+				{
+					RegisterSpareForCurrentPlayer();
+				}
+			}
+			else
+			{
+				playerScores[CurrentPlayer] += 10 - pinsRemaining;
+			}
+		}
+
+		private void AdvanceToNextAttempt()
+		{
+			currentAttempt = (currentAttempt + 1) % 2;
+		}
+
+		private void AdvanceToNextPlayer()
+		{
+			UpdateCurrentPlayerScore();
+
+			currentPlayer = (currentPlayer + 1) % playerScores.Count;
+			currentAttempt = 0;
+			pinsRemaining = 10;
+
+			if (CurrentFrameHasNoMorePlayers)
+			{
+				currentFrame++;
+			}
+		}
+
+		private void RegisterSpareForCurrentPlayer()
+		{
+			pendingBonuses[CurrentPlayer] = 1;
+			pendingBonusPoints[CurrentPlayer] = 10;
+		}
+
+		private void RegisterStrikeForCurrentPlayer()
+		{
+			pendingBonuses[CurrentPlayer] = 2;
+			pendingBonusPoints[CurrentPlayer] = 10;
+		}
+	}
+}
