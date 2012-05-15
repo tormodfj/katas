a5dc68b1f2b725b9814edbb445f683b6cb153396
Tormod <tormod.fjeldskaar@gmail.com>
2012-05-15 08:47:17 +0200

    Added Reversi Kata

---
+++ csharp/Reversi/Reversi/Reversi.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reversi
+{
+    public static class Reversi
+    {
+        public static string Run(string input)
+        {
+            char[][] parsedInput = Parse(input);
+            char[][] legalMoves = FillLegalMoves(parsedInput, '0');
+
+            return FormatBoard(legalMoves);
+        }
+
+        private static char[][] Parse(string input)
+        {
+            return input
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.ToArray())
+                .ToArray();
+        }
+
+        private static char[][] FillLegalMoves(char[][] input, char legalMoveIndicator)
+        {
+            char currentPlayer = input.Last().Last();
+            char[][] relevantInput = input.Take(8).ToArray();
+
+            return 
+                Enumerable.Range(0, 8).Select(row =>
+                    Enumerable.Range(0, 8).Select(col =>
+                        IsLegalMove(relevantInput, currentPlayer, row, col) 
+                            ? legalMoveIndicator 
+                            : input[row][col])
+                        .ToArray())
+                    .Concat(new[] { new[] { currentPlayer } })
+                    .ToArray();
+        }
+
+        private static string FormatBoard(char[][] legalMoves)
+        {
+            return string.Join(Environment.NewLine, legalMoves.Select(line => new string(line)));
+        }
+
+        private static bool IsLegalMove(char[][] input, char player, int row, int col)
+        {
+            return IsLegal(GoingN(input, row, col), player) 
+                || IsLegal(GoingNE(input, row, col), player)
+                || IsLegal(GoingE(input, row, col), player)
+                || IsLegal(GoingSE(input, row, col), player) 
+                || IsLegal(GoingS(input, row, col), player) 
+                || IsLegal(GoingSW(input, row, col), player) 
+                || IsLegal(GoingW(input, row, col), player) 
+                || IsLegal(GoingNW(input, row, col), player);
+        }
+
+        private static char[] GoingN(char[][] input, int row, int col)
+        {
+            return input.Take(row + 1).Reverse().Select(cs => cs[col]).ToArray();
+        }
+
+        private static char[] GoingNE(char[][] input, int row, int col)
+        {
+            return input.Take(row + 1).Reverse().Take(8 - col).Select(cs => cs[col++]).ToArray();
+        }
+
+        private static char[] GoingE(char[][] input, int row, int col)
+        {
+            return input[row].Skip(col).ToArray();
+        }
+
+        private static char[] GoingSE(char[][] input, int row, int col)
+        {
+            return input.Skip(row).Take(8 - col).Select(cs => cs[col++]).ToArray();
+        }
+
+        private static char[] GoingS(char[][] input, int row, int col)
+        {
+            return input.Skip(row).Select(cs => cs[col]).ToArray();
+        }
+
+        private static char[] GoingSW(char[][] input, int row, int col)
+        {
+            return input.Skip(row).Take(col + 1).Select(cs => cs[col--]).ToArray();
+        }
+
+        private static char[] GoingW(char[][] input, int row, int col)
+        {
+            return input[row].Take(col + 1).Reverse().ToArray();
+        }
+
+        private static char[] GoingNW(char[][] input, int row, int col)
+        {
+            return input.Take(row + 1).Reverse().Take(col).Select(cs => cs[col--]).ToArray();
+        }
+
+        private static bool IsLegal(char[] input, char player)
+        {
+            return IsFreeSpot(input)
+                && IsOtherPlayerAdjacent(input, player)
+                && IsPlayerBeforeDotOrEnd(input, player);
+        }
+
+        private static bool IsFreeSpot(char[] input)
+        {
+            return input.Any() && input[0] == '.';
+        }
+
+        private static bool IsOtherPlayerAdjacent(char[] input, char player)
+        {
+            return input.Skip(1).Any() && input[1] == OtherPlayer(player);
+        }
+
+        private static bool IsPlayerBeforeDotOrEnd(char[] input, char player)
+        {
+            return input.Skip(1).TakeWhile(c => c != '.').Any(c => c == player);
+        }
+
+        private static char OtherPlayer(char player)
+        {
+            var map = new Dictionary<char, char> { { 'B', 'W' }, { 'W', 'B' } };
+            return map[player];
+        }
+    }
+}
---
+++ csharp/Reversi/Reversi/ReversiKataRunner.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Reversi
+{
+    partial class ReversiKataRunner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.sample1Button = new System.Windows.Forms.Button();
+            this.sample2Button = new System.Windows.Forms.Button();
+            this.sample3Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.White;
+            this.inputTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(200, 200);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.White;
+            this.outputTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(363, 12);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(200, 200);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(251, 189);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "Run!";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // sample1Button
+            // 
+            this.sample1Button.Location = new System.Drawing.Point(251, 12);
+            this.sample1Button.Name = "sample1Button";
+            this.sample1Button.Size = new System.Drawing.Size(75, 23);
+            this.sample1Button.TabIndex = 3;
+            this.sample1Button.Text = "Sample 1";
+            this.sample1Button.UseVisualStyleBackColor = true;
+            this.sample1Button.Click += new System.EventHandler(this.sample1Button_Click);
+            // 
+            // sample2Button
+            // 
+            this.sample2Button.Location = new System.Drawing.Point(251, 41);
+            this.sample2Button.Name = "sample2Button";
+            this.sample2Button.Size = new System.Drawing.Size(75, 23);
+            this.sample2Button.TabIndex = 4;
+            this.sample2Button.Text = "Sample 2";
+            this.sample2Button.UseVisualStyleBackColor = true;
+            this.sample2Button.Click += new System.EventHandler(this.sample2Button_Click);
+            // 
+            // sample3Button
+            // 
+            this.sample3Button.Location = new System.Drawing.Point(251, 70);
+            this.sample3Button.Name = "sample3Button";
+            this.sample3Button.Size = new System.Drawing.Size(75, 23);
+            this.sample3Button.TabIndex = 5;
+            this.sample3Button.Text = "Sample 3";
+            this.sample3Button.UseVisualStyleBackColor = true;
+            this.sample3Button.Click += new System.EventHandler(this.sample3Button_Click);
+            // 
+            // ReversiKataRunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 226);
+            this.Controls.Add(this.sample3Button);
+            this.Controls.Add(this.sample2Button);
+            this.Controls.Add(this.sample1Button);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ReversiKataRunner";
+            this.Text = "Reversi Kata";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button sample1Button;
+        private System.Windows.Forms.Button sample2Button;
+        private System.Windows.Forms.Button sample3Button;
+    }
+}
+
