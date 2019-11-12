4aefef9d3bc41b85ee1ff8f8a87d2ea16eca2e44
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2019-11-12 19:49:28 +0100

    Board parsing

--- rust/Sudoku/src/main.rs
+++ rust/Sudoku/src/main.rs
@@ -3,9 +3,20 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let name = if args.len() > 0 { &args[0] } else { "VS Code Remote - Containers" };
-    println!("Hello, {}!", name);
+    let contents = fs::read_to_string(&args[0]).expect("Shit blew up");
+    let iter_of_row_strings = contents.split("\n");
+    let board: Vec<Vec<Option<u32>>> = iter_of_row_strings.map(get_row_values).collect();
+    println!("{:?}", board);
+}
+
+fn get_row_values(r:&str) -> Vec<Option<u32>> {
+    return r.chars().map(get_char_value).collect();
+}
+
+fn get_char_value(c:char) -> Option<u32> {
+    return c.to_digit(10);
 }
